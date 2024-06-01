d9cddc63a5a85ee74465c7c755a8df752e9c5e1e
Azimjon06 <ganijonovazimjon563@gmail.com>
2024-06-01 15:58:25 +0300

    add

--- src/components/Home/Hero_top/Hero_top.jsx
+++ src/components/Home/Hero_top/Hero_top.jsx
@@ -9,8 +9,8 @@ function Hero_top() {
       <div className=' mid  d-flex align-items-center justify-content-between'>
            <img src={top} alt="" />
         <div className='hero__text'>
-            <h1>Lorem ipsum dolor sit amet consectetur </h1>
-            <p> Lorem ipsum, dolor sit amet consectetur adipisicing elit. Suscipit nemo hic quos, ab, dolor aperiam nobis cum est eos error ipsum, voluptate culpa nesciunt delectus iste?</p>
+            <h1>Lorem ipsum dolor sit <br /> amet consectetur </h1>
+            <p> Lorem ipsum, dolor sit amet consectetur adipisicing elit. Suscipit nemo hic quos, ab, br dolor aperiam nobis cum est eos error ipsum, voluptate culpa nesciunt delectus iste?</p>
             <button>Learn More</button>
         </div>  
         </div>
